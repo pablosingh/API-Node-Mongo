56e03120af3cfd5a96742554503c3c5699937825
pablosingh <pablo22_4@hotmail.com>
2021-02-05 21:08:12 -0300

    2do Factoreo

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 'use strict'
-
 //Funcionalidad de Express
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const productCtrl = require('./controllers/product');
+const api = require('./routes');
 
 app.use(bodyParser.urlencoded( { extended: false} ));
 app.use(bodyParser.json());
-
-app.get( '/api/product', productCtrl.getProducts );
-app.get( '/api/product/:productId', productCtrl.getProduct );
-app.post('/api/product', productCtrl.saveProduct );
-app.put( '/api/product/:productId', productCtrl.updateProduct );
-app.delete ( '/api/product/:productId', productCtrl.deleteProduct );
+app.use('/api', api);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 
 const mongoose = require('mongoose');
 const app = require('./app');
-const port = process.env.PORT || 3001;
+const config = require('./config');
 
-mongoose.connect('mongodb://localhost:27017/shop', (err,res) => {
+mongoose.connect( config.db, (err,res) => {
 	if (err) {
 		//throw err;
 		console.log(`Error al conectar con la base de datos: ${err}`);
 	}
 	console.log('Conexion establecida con la base de datos...');
-	app.listen(port , () => {
-		console.log('API Rest corriendo en http:/localhost:'+ port);
+	
+	app.listen(config.port , () => {
+		console.log('API Rest corriendo en http:/localhost:'+ config.port);
 	});
 });
 
